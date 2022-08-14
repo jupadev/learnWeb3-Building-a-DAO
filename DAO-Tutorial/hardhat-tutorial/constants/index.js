31eaa5ab5cb3e9e18dc0cbce14e6ea71ee2f263a
Juan Pablo Vargas <jupavar@gmail.com>
2022-08-13 21:43:11 -0300

    feat: add Building a DAO

---
+++ DAO-Tutorial/hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x6726933Eca713cAe7488b310d31191cB0aa4De1f";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
